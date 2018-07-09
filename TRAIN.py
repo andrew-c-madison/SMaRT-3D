3ce22c9617df0d8f2dd338f6e34c3a973041438e
andrew-c-madison <38506056+andrew-c-madison@users.noreply.github.com>
2018-07-09 15:50:38 -0400

    Update TRAIN.py

--- TRAIN.py
+++ TRAIN.py
@@ -24,12 +24,12 @@ from sklearn.metrics import accuracy_score
 
 
 # Input and output folders
-Train_Image_Dir = os.path.join("Data", "Train", "images") 			# Path to Training images
-Train_Label_Dir = os.path.join("Data", "Train", "labels") 			# Path to Training labels
+Train_Image_Dir = os.path.join("Data", "Train", "images") 		# Path to Training images
+Train_Label_Dir = os.path.join("Data", "Train", "labels") 		# Path to Training labels
 Valid_Image_Dir = os.path.join("Data", "Validation", "images") 		# Path to Validation images 
 Valid_Labels_Dir = os.path.join("Data", "Validation", "labels") 	# Path to Validation labels  
-logs_dir = os.path.join("logs") 									# Path to log directory
-TrainLossTxtFile = os.path.join(logs_dir, "TrainLoss.txt") 			# Path to Training loss record
+logs_dir = os.path.join("logs") 					# Path to log directory
+TrainLossTxtFile = os.path.join(logs_dir, "TrainLoss.txt") 		# Path to Training loss record
 ValidRecTxtFile = os.path.join(logs_dir, "ValidationRecord.txt")	# Path to Validation record
 
 
@@ -41,16 +41,16 @@ CheckVGG16Model.CheckVGG16(model_path) 			# Check vgg16 model, download if not p
 
 # Define Hyperparameters 
 NUM_CLASSES = 6 		# Number of classes
-Batch_Size = 10			# Number of files per training iteration
+Batch_Size = 5			# Number of files per training iteration
+Validation_Batch_Size = 5	# Number of files per validation iteration
 UseValidationSet = True 	# Validation flag
-learning_rate = 1e-5 		# Learning rate for Adam Optimizer
-Weight_Loss_Rate = 5e-4		# Weight for the weight decay loss function
+UseStochasticity = True		# Apply random noise and transformations to training images
+learning_rate = 1e-5		# Learning rate for Adam Optimizer
 MAX_ITERATION = int(100010) 	# Max  number of training iteration
-
 
 # Solver for Model
 def train(loss_val, var_list):
-	optimizer = tf.train.AdamOptimizer(learning_rate)
+	optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=0.9, beta2=0.999, epsilon=1e-08, use_locking=False)	
 	grads = optimizer.compute_gradients(loss_val, var_list=var_list)
 	return optimizer.apply_gradients(grads)
 
@@ -67,8 +67,8 @@ def main(argv=None):
 	Net.build(image, NUM_CLASSES, keep_prob) # Create the net and load intial weights
 
 	# Get loss functions for neural net work one loss function for each set of labels
-	Loss = tf.reduce_mean((tf.nn.sparse_softmax_cross_entropy_with_logits(labels=tf.squeeze(GTLabel, \
-				squeeze_dims=[3]), logits=Net.Prob,name="Loss")))  # Define loss function for training
+	Loss = -tf.reduce_sum(tf.multiply(tf.to_float(tf.one_hot(tf.squeeze(GTLabel, squeeze_dims=[3]), NUM_CLASSES)), \
+				tf.log(tf.nn.softmax(Net.Prob) + 1e-12)))
 
 	# Create solver for the net
 	trainable_var = tf.trainable_variables() # Collect all trainable variables for the net
@@ -76,12 +76,12 @@ def main(argv=None):
 
 	# Create reader for training data
 	TrainReader = Data_Reader.Data_Reader(Train_Image_Dir, \
-			GTLabelDir=Train_Label_Dir, BatchSize=Batch_Size)
+				GTLabelDir=Train_Label_Dir, BatchSize=Batch_Size)
     
 	# Create reader for validation data
 	if UseValidationSet:
 		ValidReader = Data_Reader.Data_Reader(Valid_Image_Dir, \
-				GTLabelDir=Valid_Labels_Dir, BatchSize=Batch_Size) 
+				GTLabelDir=Valid_Labels_Dir, BatchSize=Validation_Batch_Size) 
 
 	# Start TensorFlow session
 	sess = tf.Session() 
@@ -110,7 +110,10 @@ def main(argv=None):
 
 	# Start Training loop: Main Training
 	for itr in range(MAX_ITERATION):
-		Images, GTLabels = TrainReader.ReadAndAugmentNextBatch() # Load images and labels for training
+		if UseStochasticity:
+			Images, GTLabels = TrainReader.ReadAndAugmentNextBatch() # Load images and labels
+		else:
+			Images, GTLabels = TrainReader.ReadNextBatchClean()
 		feed_dict = {image: Images,GTLabel:GTLabels, keep_prob: 0.5}
 		sess.run(train_op, feed_dict=feed_dict) # Train one cycle
 		
@@ -131,7 +134,7 @@ def main(argv=None):
 				f.close()
 
 		# Write and display Validation Set Loss 
-		if UseValidationSet and itr % 10 == 0:
+		if UseValidationSet and itr % 25 == 0:
 			SumAcc = np.float64(0.0)
 			SumLoss = np.float64(0.0)
 			NBatches = np.int(np.ceil(ValidReader.NumFiles/ValidReader.BatchSize))
@@ -139,6 +142,7 @@ def main(argv=None):
 			for i in range(NBatches): 
 				Images, GTLabels= ValidReader.ReadNextBatchClean() # load validation data
 				feed_dict = {image: Images,GTLabel: GTLabels, keep_prob: 1.0}
+				
 				# Calculate loss for all labels set
 				TLoss = sess.run(Loss, feed_dict=feed_dict)
 				SumLoss += TLoss
@@ -154,9 +158,10 @@ def main(argv=None):
 			SumAcc/=NBatches
 			SumLoss/=NBatches
 			print("Validation Loss: " + str(SumLoss))
+			print("Validation Accuracy: " + str(SumAcc))
 			with open(ValidRecTxtFile, "a") as f:
 				f.write("\n" + str(itr) + "\t" + str(SumLoss) + "\t" + str(SumAcc))
-				f.close()		
+				f.close()			
 
 
 if __name__ == "__main__":
